a4bb1f4ed54fbbd2cfa03b1d3df89a27f80d97e8
Dey-Sumit <sumax333@gmail.com>
2021-10-14 12:25:02 +0530

    fixed types

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -1,24 +1,25 @@
-import { FC, MouseEventHandler } from "react";
+import { FC } from "react";
 import { isOperand } from "src/utils";
 
 const getStyles = (value: string, currentOperator: string) => {
-  let styles = "col-span-3 p-5 border border-gray-600 grid place-items-center";
-  if (currentOperator === value) styles = `${styles} border-green-500`;
+  let styles = "col-span-3 p-5 border border-gray-600 grid place-items-center"; // base style
+  if (currentOperator === value) styles = `${styles} border-green-500`; // active element
 
   if (value === "0") styles = `${styles} col-span-6`;
 
   if (isOperand(value) || value == ".") {
     styles = `${styles} bg-gray-800`;
   } else {
+    // top row
     if (["%", "+/-", "AC"].includes(value)) styles = `${styles} bg-gray-700`;
-    else styles = `${styles} bg-yellow-500`;
+    else styles = `${styles} bg-yellow-500`; // right col
   }
   return styles;
 };
 
 interface Props {
   value: string;
-  clickHandler: any;
+  clickHandler: (value: string) => void;
   currentOperator: string;
 }
 
